8d372e1ddf2c2d984ce60cba9c5f3386deac3fcf
Maximilian Dahlquist <max.dahlquist@hva.nl>
2026-04-07 15:47:25 +0200

    Added date to report files (to stop overwrites), and puts them in the directory "reports".

--- newman.ts
+++ newman.ts
@@ -1,8 +1,19 @@
 import newman from "newman";
 import * as fs from 'fs';
+import path from 'path';
 
 const collection = process.argv[2]
 const iterations = parseInt(process.argv[3], 10) || 1;
+const dir = path.join(__dirname, 'reports');
+
+function createDir(){
+    if(!fs.existsSync(dir)){
+        console.log(`Directory ${dir} does not exist, creating...`);
+        fs.mkdirSync(dir);
+    }
+}
+
+createDir();
 
 interface Results {
     responseTime: number;
@@ -35,7 +46,17 @@ newman.run({
     results.push(args.response.responseTime)
 
 }).on('done', () => {
-    fs.writeFileSync("response-times.json", JSON.stringify(results, null, 2));
+    let dateTime = new Date();
+
+    if(collection.includes("SQL")){
+        fs.writeFileSync(dir+`/response-times-sql.json - ${dateTime}`, JSON.stringify(results, null, 2));
+    } else if(collection.includes("Prisma")){
+        fs.writeFileSync(dir+`/response-times-prisma.json - ${dateTime}`, JSON.stringify(results, null, 2));
+    } else if(collection.includes("Sequelize")){
+        fs.writeFileSync(dir+`/response-times-sequelize.json - ${dateTime}`, JSON.stringify(results, null, 2));
+    } else{
+        console.log("Incorrect collection naming scheme, please include 'SQL', 'Prisma', or 'Sequelize' in the collection name. ex: 'SQL-get-simple.postman_collection'");
+    }
 
     console.log("Test completed");
 })
