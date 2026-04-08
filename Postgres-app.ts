a0210b4171c39070c2301a5fd11889f6f20fc505
Maximilian Dahlquist <max.dahlquist@hva.nl>
2026-04-08 19:01:27 +0200

    updated postgres-app to include memory tracker functionality

--- Postgres-app.ts
+++ Postgres-app.ts
@@ -1,8 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { MemoryUsageChecker } from './memory_usage'
 
+dotenv.config();
 
 const app = express();
 const PORT = 8090;    // the port used by the website
+const memoryChecker = new MemoryUsageChecker;
+const memoryTracker = memoryChecker.initialize();
+
+memoryChecker.createFile();
+
+app.use(express.json());
+
+// Memory tracker middleware (memory check after request is complete)
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        const url = req.originalUrl.toLocaleLowerCase();
+
+        if(url.includes("prisma") || url.includes("sql")){
+            memoryTracker();
+        }
+    });
+    next();
+});
 
 app.use(express.json());
 
