71ea530dca8a87b22bd0cf2a8cf914639a2e56df
Maximilian Dahlquist <max.dahlquist@hva.nl>
2026-04-08 14:06:09 +0200

    updated file naming to be more dynamic

--- newman.ts
+++ newman.ts
@@ -1,10 +1,15 @@
 import newman from "newman";
 import * as fs from 'fs';
 import path from 'path';
+import { MemoryUsageChecker } from "./memory_usage";
+import { json } from "sequelize";
 
 const collection = process.argv[2]
 const iterations = parseInt(process.argv[3], 10) || 1;
 const dir = path.join(__dirname, 'reports');
+const memoryReader = new MemoryUsageChecker();
+
+let output = '';
 
 function createDir(){
     if(!fs.existsSync(dir)){
@@ -13,7 +18,25 @@ function createDir(){
     }
 }
 
+function ValidateOutput(){
+    if(!collection.includes("SQL") && !collection.includes("Prisma") && !collection.includes("Sequelize")){
+        console.error("Incorrect collection naming scheme, please include 'SQL', 'Prisma', or 'Sequelize' in the collection name. ex: 'SQL-get-simple.postman_collection'");
+        process.exit();
+    } else{
+        const dateTime = new Date();
+        const hours = String(dateTime.getHours()).padStart(2, '0');
+        const minutes = String(dateTime.getMinutes()).padStart(2, '0');
+        const seconds = String(dateTime.getSeconds()).padStart(2, '0');
+        const cleanTime = `${hours}-${minutes}-${seconds}`
+
+        const collectionName = path.basename(collection);
+        
+        output = path.join(dir+`/response-times-${collectionName}-${iterations}-${cleanTime}.json`);
+    }
+}
+
 createDir();
+ValidateOutput();
 
 interface Results {
     responseTime: number;
@@ -21,21 +44,12 @@ interface Results {
 
 const results: Results[] = [];
 
+memoryReader.clearMemoryUsage();
+
 newman.run({
     collection: collection,
     iterationCount: iterations,
-    reporters: 'cli',
-    reporter: {
-        cli: {
-        noSummary: true
-    }
-}
 }).on('request', (e: Error | null, args: any) => {
-    if(!collection.includes("SQL") && !collection.includes("Prisma") && !collection.includes("Sequelize")){
-        console.error("Incorrect collection naming scheme, please include 'SQL', 'Prisma', or 'Sequelize' in the collection name. ex: 'SQL-get-simple.postman_collection'");
-        process.exit();
-    }
-
     if(e){
         console.error("Request failed: ", e);
         return;
@@ -44,28 +58,13 @@ newman.run({
     const firstRequest = args.cursor.iteration === 0 && args.cursor.position === 0;
 
     if(firstRequest){
-        console.log("Dropping first request");
+        console.log("Running collection and dropping first request...");
         return;
     }
 
-    results.push(args.response.responseTime)
+    results.push({responseTime: args.response.responseTime});
 
 }).on('done', () => {
-    const dateTime = new Date();
-    const hours = String(dateTime.getHours()).padStart(2, '0');
-    const minutes = String(dateTime.getMinutes()).padStart(2, '0');
-    const seconds = String(dateTime.getSeconds()).padStart(2, '0');
-    const cleanTime = `${hours}-${minutes}-${seconds}`
-
-    if(collection.includes("SQL")){
-        fs.writeFileSync(dir+`/response-times-sql-${cleanTime}.json`, JSON.stringify(results, null, 2));
-    } else if(collection.includes("Prisma")){
-        fs.writeFileSync(dir+`/response-times-prisma-${cleanTime}.json`, JSON.stringify(results, null, 2));
-    } else if(collection.includes("Sequelize")){
-        fs.writeFileSync(dir+`/response-times-sequelize-${cleanTime}.json`, JSON.stringify(results, null, 2));
-    } else{
-        console.error("Incorrect collection naming scheme, please include 'SQL', 'Prisma', or 'Sequelize' in the collection name. ex: 'SQL-get-simple.postman_collection'");
-    }
-
-    console.log(`Test completed, report created in directory: ${dir}`);
+    fs.writeFileSync(output, JSON.stringify(results, null, 2));
+    console.log(`Test completed, report ${output} created in directory: ${dir}`);
 })
