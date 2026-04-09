de7148cea9e0540521edfd2317a75350d3b326a6
Pistolmannen <erikgustfasson03@gmail.com>
2026-04-09 17:02:01 +0200

    some simple pure sql for sequelize

--- postgres-program/sequelize-controllers/sequelize-simple-controller.ts
+++ postgres-program/sequelize-controllers/sequelize-simple-controller.ts
@@ -2,21 +2,15 @@ import { sequelize } from '../sequelize-db';
 import { Owner, Type, Pet, Visit, Vet, Specialty, Vet_Specialty, PetWowner, VisitWpet } from '../sequelize-schema';
 import { Request, Response } from "express";
 
-export const SimpleSelect = async (req: Request, res: Response) => {    // Get every pet from "Madison"
+// "SELECT Pets.id, Pets.name, Pets.birth_date, Pets.type_id, Pets.owner_id, Owners.city FROM Pets JOIN Owners ON pets.owner_id = Owners.id WHERE Owners.city = 'Madison'"
+
+export const SimpleSelect = async (req: Request, res: Response) => {    // Get every pet
   try {
-    const simpleSelect = await Pet.findAll({
-      include: [{
-        model: Owner,
-        where : {
-          city: 'Madison',
-        },
-        required: true
-      }]
-    });
+    const simpleSelect = await Pet.findAll();
     
     res.json({
       simpleSelect,
-    })
+    });
   }
   catch (err) {
     console.log(err);
@@ -26,11 +20,11 @@ export const SimpleSelect = async (req: Request, res: Response) => {    // Get e
 
 export const SimpleSelectPure = async (req: Request, res: Response) => {
   try {
-    const [results, metadata] = await sequelize.query("SELECT Pets.id, Pets.name, Pets.birth_date, Pets.type_id, Pets.owner_id, Owners.city FROM Pets JOIN Owners ON pets.owner_id = Owners.id WHERE Owners.city = 'Madison'") // 
+    const [results, metadata] = await sequelize.query("SELECT * FROM pets");
 
     res.json({
       results,
-    })
+    });
   }
   catch (err) {
     console.log(err);
@@ -39,7 +33,7 @@ export const SimpleSelectPure = async (req: Request, res: Response) => {
 
 export const SimpleCreate = async (req: Request, res: Response) => {    // Create owner "Deller"
   try {
-    const simpleCreate = await Owner.create({
+    const simpleCreate = await Owner.create({ 
       first_name: "Deller",
       last_name: "Menser",
       address: "notwere st",
@@ -49,7 +43,22 @@ export const SimpleCreate = async (req: Request, res: Response) => {    // Creat
 
     res.json({
       simpleCreate,
-    })
+    });
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+};
+
+export const SimpleCreatePure = async (req: Request, res: Response) => {    // Create owner "Deller"
+  try {
+
+    const [results, metadata] = await sequelize.query("INSERT INTO owners (first_name, last_name, address, city, telephone) VALUES ('Deller', 'Menser', 'notwere st', 'Entier', '0000666000')");
+
+    res.json({
+      results,
+    });
   }
   catch (err) {
     console.log(err);
@@ -59,13 +68,13 @@ export const SimpleCreate = async (req: Request, res: Response) => {    // Creat
 
 export const SimpleDelete = async (req: Request, res: Response) => {    // Delete the first owner called "Deller"
   try {
-    const Deller = await Owner.findOne({
+    const Deller = await Owner.findOne({    // Deller = "SELECT * FROM owners WHERE first_name = 'Deller' LIMIT 1"
       where: {
         first_name: "Deller"
       }
     });
 
-    const simpleDelete = await Owner.destroy({
+    const simpleDelete = await Owner.destroy({    // "DELETE FROM owners WHERE id = Deller.id"
       where: {
         id: Deller?.get("id"),
       }
@@ -83,14 +92,14 @@ export const SimpleDelete = async (req: Request, res: Response) => {    // Delet
 
 export const SimpleUpdate = async (req: Request, res: Response) => {    // Update the city of the first owner called "Deller"
   try {
-    const oldDeller = await Owner.findOne({
+    const oldDeller = await Owner.findOne({   // Deller = "SELECT * FROM owners WHERE first_name = 'Deller' AND city = 'Entier' LIMIT 1"
       where: {
         first_name: "Deller",
         city: "Entier",
       }
     });
     
-    const simpleUpdate = await Owner.update(
+    const simpleUpdate = await Owner.update(    // "UPDATE owners SET city = 'Exiter' WHERE id = Deller.id"
       {
         city: "Exiter"
       },
--- postgres-program/sequelize-router.ts
+++ postgres-program/sequelize-router.ts
@@ -1,29 +1,30 @@
 import express from 'express'
 import { GetEverything, AllElement, AllOwners, AllOwnersPure } from './sequelize-controllers/sequelize-old-controller'
-import { SimpleSelect, SimpleSelectPure, SimpleCreate, SimpleDelete, SimpleUpdate  } from './sequelize-controllers/sequelize-simple-controller';
+import { SimpleSelect, SimpleSelectPure, SimpleCreate, SimpleCreatePure, SimpleDelete, SimpleUpdate  } from './sequelize-controllers/sequelize-simple-controller';
 import { AdvancedSelect, AdvancedCreate, AdvancedDelete, AdvancedUpdate } from './sequelize-controllers/sequelize-advanced-controller';
 
 const sequelizeRouter = express.Router();
 
 // Everything
-sequelizeRouter.get("/", GetEverything)
-sequelizeRouter.get("/AllElement", AllElement)
+sequelizeRouter.get("/", GetEverything);
+sequelizeRouter.get("/AllElement", AllElement);
 
 // Owners
-sequelizeRouter.get("/AllOwners", AllOwners)
-sequelizeRouter.get("/AllOwnersPure", AllOwnersPure)
+sequelizeRouter.get("/AllOwners", AllOwners);
+sequelizeRouter.get("/AllOwnersPure", AllOwnersPure);
 
 // Simple
-sequelizeRouter.get("/Simple/Select", SimpleSelect)
-sequelizeRouter.get("/Simple/Select/Pure", SimpleSelectPure)
-sequelizeRouter.get("/Simple/Create", SimpleCreate)
-sequelizeRouter.get("/Simple/Delete", SimpleDelete)
-sequelizeRouter.get("/Simple/Update", SimpleUpdate)
+sequelizeRouter.get("/Simple/Select", SimpleSelect);
+sequelizeRouter.get("/Simple/Select/Pure", SimpleSelectPure);
+sequelizeRouter.get("/Simple/Create", SimpleCreate);
+sequelizeRouter.get("/Simple/Create/Pure", SimpleCreatePure);
+sequelizeRouter.get("/Simple/Delete", SimpleDelete);
+sequelizeRouter.get("/Simple/Update", SimpleUpdate);
 
 // Advanced
-sequelizeRouter.get("/Advanced/Select", AdvancedSelect)
-sequelizeRouter.get("/Advanced/Create", AdvancedCreate)
-sequelizeRouter.get("/Advanced/Delete", AdvancedDelete)
-sequelizeRouter.get("/Advanced/Update", AdvancedUpdate)
+sequelizeRouter.get("/Advanced/Select", AdvancedSelect);
+sequelizeRouter.get("/Advanced/Create", AdvancedCreate);
+sequelizeRouter.get("/Advanced/Delete", AdvancedDelete);
+sequelizeRouter.get("/Advanced/Update", AdvancedUpdate);
 
-module.exports = sequelizeRouter
+module.exports = sequelizeRouter;
