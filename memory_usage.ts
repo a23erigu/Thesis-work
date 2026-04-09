966d2739d9175e957b0bbf3aea00f365b492c04b
Maximilian Dahlquist <max.dahlquist@hva.nl>
2026-04-09 13:31:25 +0200

    Added comments to memory usage code

--- memory_usage.ts
+++ memory_usage.ts
@@ -5,18 +5,22 @@ import * as path from 'path'
 export class MemoryUsageChecker{
     private fileName = path.join(__dirname, 'memoryReadings.txt');
 
+    // Initalizer function to ensure the program is running correctly with all prerequisites
     public initialize(){
         console.log(`target file: ${this.fileName}`);
 
+        // Check if explicit gargabe collector is enabled
         if(!global.gc){
             console.error("Garbage collector not enabled, add '--expose-gc' to the launch options and try again");
             process.exit();
         } else {
-            console.log("Garbage collector running!");
+            console.log("Garbage collector enabled!");
         }
 
+        // Get memory usage at start to compare with after request is finished
         const init = this.getMemoryUsage();
 
+        // Return the memory usage after request and add to memoryReadings.txt file
         return async () => {
             const reading = this.getMemoryUsage() - init;
 
@@ -24,24 +28,28 @@ export class MemoryUsageChecker{
         }
     }
 
+    // Create the memory logging file if it doesn't exist
     public createFile(){
         if(!fss.existsSync(this.fileName)){
             console.log(`Directory ${this.fileName} does not exist, creating...`);
             fss.open(this.fileName, 'w', function(e){
                 if(e) console.log("Could not create file: ", e);
-                else console.log("Create file");
+                else console.log("Created file");
             });
         }
     }
 
+    // Clear the file contents so that they don't spill over to the next measurement
     public async clearMemoryUsage(){
         await fs.writeFile(this.fileName, '');
     }
 
+    // Add the reading to the file with 3 decimal points
     private async appendToFile(reading: number){
         await fs.appendFile(this.fileName, `${reading.toFixed(3)},`);
     }
 
+    // Get the current memory usage for the heap in MB
     public getMemoryUsage(){
         const memoryUsed = process.memoryUsage();
         const usedHeapMB = memoryUsed.heapUsed / 1024 / 1024;
@@ -49,6 +57,7 @@ export class MemoryUsageChecker{
         return usedHeapMB;
     }
 
+    // Get every memory reading made during a test and map it to each request in newman.ts
     public async getTotalMemoryUsage(): Promise<Number[]>{
         try{
             const data = await fs.readFile(this.fileName, 'utf-8');
