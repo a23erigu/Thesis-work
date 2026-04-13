d931a2fd33185853ee1a47a0e1b5db9a8e4b87db
Pistolmannen <erikgustfasson03@gmail.com>
2026-04-13 15:30:13 +0200

    advanced CRUD added to router and advanced select made

---
+++ postgres-program/sql/sql-controllers/sql-advanced-controller.ts
@@ -0,0 +1,93 @@
+import pool from "../sql-db-postgres";
+import { Request, Response } from "express";
+
+export const AdvancedSelect = async (req: Request, res: Response) => {      // Select all users that have a hamster who have hade a visit
+  let conn;
+  try {
+    conn = await pool.connect()
+
+    const results = await conn.query("SELECT owners.id, owners.first_name, owners.last_name, owners.address, owners.city, owners.telephone FROM owners JOIN pets ON owners.id = pets.owner_id JOIN visits ON pets.id = visits.pet_id JOIN types ON pets.type_id = types.id WHERE types.name = 'hamster'"); 
+    const rows = results["rows"];
+
+    res.json({
+      rows,
+    });
+  }
+  catch (err) {
+    console.log(err);
+  } finally{
+    if(conn){
+        conn.release();
+    }
+  }
+
+};
+
+export const AdvancedCreate = async (req: Request, res: Response) => { 
+  let conn;
+  try {
+    conn = await pool.connect()
+
+    const results = await conn.query("");
+
+    res.json({
+      results,
+    });
+  }
+  catch (err) {
+    console.log(err);
+  } finally{
+    if(conn){
+        conn.release();
+    }
+  }
+
+};
+
+export const AdvancedDelete = async (req: Request, res: Response) => {   
+  let conn;
+  try {
+    conn = await pool.connect()
+
+    const Deller = await conn.query("");
+    const id = Deller["rows"][0]["id"];
+
+    const result = await conn.query();
+
+    res.json({
+      result,
+    });
+  }
+  catch (err) {
+    console.log(err);
+  } finally{
+    if(conn){
+        conn.release();
+    }
+  }
+
+};
+
+export const AdvancedUpdate = async (req: Request, res: Response) => { 
+  let conn
+  try {
+    conn = await pool.connect()
+
+    const oldDeller = await conn.query("");
+    const id = oldDeller["rows"][0]["id"];
+
+    const result = await conn.query();
+
+    res.json({
+      result,
+    });
+  }
+  catch (err) {
+    console.log(err);
+  } finally{
+    if(conn){
+        conn.release();
+    }
+  }
+
+};
